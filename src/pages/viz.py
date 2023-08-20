6e986fe8c093f612ffebfa784f8f06b5e4f67399
Alex <alexbriody@gmail.com>
2023-08-20 18:15:51 -0400

    commit without secrets

--- src/pages/viz.py
+++ src/pages/viz.py
@@ -11,13 +11,23 @@ from io import StringIO
 filepath = os.path.join(Path(__file__).parents[1], 'data/oracle_cards.csv')
 df = pd.read_csv(filepath, low_memory=False)
 
-# Set up connection to Tableau 
-tableau_auth = TSC.PersonalAccessTokenAuth(
-    st.secrets["tableau"]["token_name"],
-    st.secrets["tableau"]["token_secret"],
-    st.secrets["tableau"]["site_id"]
-)
-server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+
+# # Load secrets from Streamlit's secrets manager
+# tableau_secrets = st.secrets.get("tableau")
+
+# # Check if secrets were loaded successfully
+# if tableau_secrets is None:
+#     st.error("Tableau secrets not found. Make sure you have added them to the Streamlit secrets manager.")
+#     st.stop()
+
+# # Set up connection to Tableau 
+# tableau_auth = TSC.PersonalAccessTokenAuth(
+#     st.secrets["tableau"]["token_name"],
+#     st.secrets["tableau"]["token_secret"],
+#     st.secrets["tableau"]["site_id"]
+# )
+# server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+
 
 
 # Take in a user input:
@@ -32,7 +42,7 @@ if type_vis == 'scatterplot':
         try:
             st.plotly_chart(px.scatter(df, x=answer, y=answer2, hover_data=['name']), use_container_width=True)
         except BaseException:
-            print("Error visualizing this column")
+            print("Error visualizing this column for scatterplot")
 
 if type_vis == 'histogram':
     # Get the column for the histogram
@@ -42,7 +52,7 @@ if type_vis == 'histogram':
         try:
             st.plotly_chart(px.histogram(df, x=answer_hist), use_container_width=True)
         except BaseException:
-            print("Error visualizing this column")
+            print("Error visualizing this column for historgram")
 
 if type_vis == 'bar chart':
     # Get the columns for the bar chart
@@ -53,22 +63,36 @@ if type_vis == 'bar chart':
         try:
             st.plotly_chart(px.bar(df, x=answer_bar_x, y=answer_bar_y), use_container_width=True)
         except BaseException:
-            print("Error visualizing this column")
+            print("Error visualizing this column for bar chart")
 
-          
+
 # Create the Plotly bar chart
 fig = px.bar(df, x="country", y="monthly_revenue")
-            
-# Create a workbook and add the visualization to it
-try:
-    with server.auth.sign_in(tableau_auth):
-        workbook = TSC.WorkbookItem(name='Streamlit Bar Chart')
-        workbook = server.workbooks.publish(workbook)
 
-        view_item = TSC.ViewItem(workbook.id)
-        server.views.populate_image(view_item, content=fig.to_image(format='png'))
+html_path = "plotly_chart.html"
+fig.write_html(html_path)
 
-    st.success("Bar chart visualization has been published to Tableau!")
+# # Set up connection to Tableau Server
+# tableau_server_url = 'https://prod-useast-b.online.tableau.com/#/site/alexbriody/views/streamlit_write_page/streamlit_write_page?:iid=7'
+# auth = TSC.TableauAuth('alexbriody@gmail.com', 'Jiachi67!')  
 
-except BaseException as e:
-    st.error("Error:", e)
+# # Connect to the server
+# with server.auth.sign_in(auth):
+#     # Create a new workbook
+#     new_workbook = TSC.WorkbookItem('Streamlit Bar Chart Workbook')
+
+#     # Add a view to the workbook
+#     new_view = TSC.ViewItem()
+
+#     # Read the HTML content
+#     with open(html_path, 'r') as html_file:
+#         html_content = html_file.read()
+
+#     # Set the HTML content
+#     new_view.set_html(html_content)
+
+#     # Publish the workbook with the view
+#     server.workbooks.publish(new_workbook, [new_view], overwrite=True)
+
+# st.success("Bar chart visualization has been published to Tableau!")
+
